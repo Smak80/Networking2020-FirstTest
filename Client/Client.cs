04e23c0f9b470d6912b829748f1a6dbb26ed8116
Smak80 <smak-80@yandex.ru>
2020-05-05 10:03:48 +0300

    Клиент-серверный диалог

--- Client/Client.cs
+++ Client/Client.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
@@ -20,11 +21,73 @@ namespace Client
                 Console.WriteLine("Подключение к {0}", this.serverHost);
                 cSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
                 cSocket.Connect(this.serverHost, port);
+                new Thread(() =>
+                {
+                    Communicate();
+                }).Start();
+                while (true)
+                {
+                    String userData = "";
+                    userData = Console.ReadLine();
+                    SendData(userData);
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Что-то пошло не так... :(");
             }
+        }
+
+        public void SendData(String data)
+        {
+            if (cSocket != null)
+            {
+                try
+                {
+                    if (data.Trim().Equals("")) return;
+                    var b = Encoding.UTF8.GetBytes(data);
+                    Console.WriteLine("Отправка сообщения...");
+                    cSocket.Send(b);
+                    Console.WriteLine("Сообщение успешно отправлено!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Не удалось отправить сообщение :(");
+                }
+            }
+        }
+
+        void Communicate()
+        {
+            if (cSocket != null)
+            {
+                Console.WriteLine("Начало общения с клиентом");
+                while (true)
+                {
+                    String d = ReceiveData();
+                    Console.WriteLine("Сервер ответил: {0}", d);
+                }
+            }
+        }
+
+        String ReceiveData()
+        {
+            String res = "";
+            if (cSocket != null)
+            {
+                try
+                {
+                    var b = new byte[65536];
+                    Console.WriteLine("Ожидание данных от клиента...");
+                    var cnt = cSocket.Receive(b);
+                    Console.WriteLine("Данные от клиента успешно получены");
+                    res = Encoding.UTF8.GetString(b, 0, cnt);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("Не удалось получить данные от клиента :(");
+                }
+            }
+            return res;
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -4,12 +4,80 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server
 {
     class Server
     {
+        class ConnectedClient
+        {
+            public Socket cSocket;
+            public static List<ConnectedClient> clients = new List<ConnectedClient>();
+
+            public ConnectedClient(Socket s)
+            {
+                cSocket = s;
+                clients.Add(this);
+                new Thread(() =>
+                {
+                    Communicate();
+                }).Start();
+            }
+            void Communicate()
+            {
+                if (cSocket != null)
+                {
+                    Console.WriteLine("Начало общения с клиентом");
+                    while (true)
+                    {
+                        String d = ReceiveData();
+                        Console.WriteLine("Клиент передал: {0}", d);
+                        SendData("Спасибо тебе! Твои данные получил!");
+                    }
+                }
+            }
+
+            String ReceiveData()
+            {
+                String res = "";
+                if (cSocket != null)
+                {
+                    try
+                    {
+                        var b = new byte[65536];
+                        Console.WriteLine("Ожидание данных от клиента...");
+                        var cnt = cSocket.Receive(b);
+                        Console.WriteLine("Данные от клиента успешно получены");
+                        res = Encoding.UTF8.GetString(b, 0, cnt);
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine("Не удалось получить данные от клиента :(");
+                    }
+                }
+                return res;
+            }
+
+            public void SendData(String data)
+            {
+                if (cSocket != null)
+                {
+                    try
+                    {
+                        if (data.Trim().Equals("")) return;
+                        var b = Encoding.UTF8.GetBytes(data);
+                        Console.WriteLine("Отправка сообщения...");
+                        cSocket.Send(b);
+                        Console.WriteLine("Сообщение успешно отправлено!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Не удалось отправить сообщение :(");
+                    }
+                }
+            }
+        }
         private String host;
         private Socket sSocket;
         private const int port = 8034;
@@ -42,8 +110,9 @@ namespace Server
                 while (true)
                 {
                     Console.WriteLine("Ожидание нового подключения...");
-                    Socket cSocket = sSocket.Accept();
+                    var cSocket = sSocket.Accept();
                     Console.WriteLine("Соединение с клиентом установлено!");
+                    new ConnectedClient(cSocket);
                 }
             }
             catch (Exception e)
